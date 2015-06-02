c266f297db0428d43b8b3763f65a8161a59ad266
2ez.mihaipetrescu_2ez <mihai.petrescu@webmanage.com>
2015-06-02 18:49:01 +1000

    removing some dead code

--- BrainThemeBootstrap.php
+++ BrainThemeBootstrap.php
@@ -13,45 +13,6 @@ use yii\base\Event;
 
 class BrainThemeBootstrap implements BootstrapInterface {
     public function bootstrap($app){
-       //\Yii::$classMap = array_merge(\Yii::$classMap,[
-            //'yii\grid\CheckboxColumn'=>'@yii/adminUi/widget/CheckboxColumn.php',
-            //'yii\grid\ActionColumn'=>'@theme/widgets/ActionColumn.php',
-        //]);
-        /*
-        $app->set('view', [
-            'class'=>'yii\web\View',
-            'theme' => [
-                'pathMap' => ['@backend/views' => '@backend/themes/adminui'],   // for Admin theme which resides on extension/adminui
-                'baseUrl' => '@web/themes/adminui',
-            ],
-        ]);
-
-        Yii::setAlias('theme', __DIR__ . '/themes');
-
-        $app->set('assetManager' , [
-            'class'	=> 'yii\web\AssetManager',
-            'bundles' => [
-                'yii\widgets\ActiveFormAsset' => [
-                    'js' => [],
-                    'depends' => [
-                        'yii\adminUi\assetsBundle\AdminUiActiveForm',
-                    ],
-                ],
-                'yii\grid\GridViewAsset' => [
-                    'depends'   => [
-                        'backend\assets\AppAsset'
-                    ],
-                ],
-            ],
-            'linkAssets' => true,
-        ]);
-
-        Event::on(Controller::className(), Controller::EVENT_BEFORE_ACTION, function ($event) {
-            if(in_array($event->action->id,['login','forgot','reset-password']) && in_array('backend',  explode("\\", $event->sender->className()))){
-                $event->sender->layout = '//blank';
-            }
-        });
-        */
         if(Yii::$app->id == "app-backend") {
             Yii::$container->set('yii\bootstrap\ActiveForm',
                 [
